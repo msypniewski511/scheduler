c1e839c6c812eebf96bacaab58e93313d8c09a4b
Maciej Sypniewski <msypniewski511@gmail.com>
2020-07-16 10:18:02 +0100

    Added boootstrap icons with helper and primary button heaing

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,17 @@ module ApplicationHelper
     gravatar_id = Digest::MD5::hexdigest(user.email).downcase
     "http://gravatar.com/avatar/#{gravatar_id}.png"
   end
+
+
+  # https://stackoverflow.com/questions/60727460/using-bootstrap-icons-in-rails-6-with-webpacker
+  
+  def icon(icon, options = {})
+    file = File.read("node_modules/bootstrap-icons/icons/#{icon}.svg")
+    doc = Nokogiri::HTML::DocumentFragment.parse file
+    svg = doc.at_css 'svg'
+    if options[:class].present?
+      svg['class'] += " " + options[:class]
+    end
+      doc.to_html.html_safe
+  end
 end
